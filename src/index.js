a1e127e994511a7a502a14f3e95fc01f2faa5713
Denis Coder <dnxcoder@gmail.com>
2022-12-20 13:36:20 -0600

    statement by date

--- src/index.js
+++ src/index.js
@@ -113,4 +113,21 @@ app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
   });
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  console.log(dateFormat);
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
 app.listen(3333);
