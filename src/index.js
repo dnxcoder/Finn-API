aa07f2f11411aed3d6cc809bd19d967e41454c0b
Denis Coder <dnxcoder@gmail.com>
2022-12-19 19:16:35 -0600

    stared project, and created new route create

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const app = express();
+
+app.use(express.json());
+
+const costumers = [];
+
+/*
+cpf - string
+name - string
+id - uuid
+*/
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const id = uuidv4();
+
+  costumers.push({
+    id,
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.listen(3333);
