443d4a291e6006b1e2eeba8842b10dce35f6d0db
Denis Coder <dnxcoder@gmail.com>
2022-12-20 14:13:20 -0600

    update account

--- src/index.js
+++ src/index.js
@@ -130,4 +130,19 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(statement);
 });
 
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.send({ customer });
+});
+
 app.listen(3333);
